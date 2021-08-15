85d9672615be8d98be95d0c2a1cd1d850c30cf67
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-08-15 14:46:06 -0600

    created HTTP methods

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express();
 // server.use(helmet());
 server.use(express.json());
 
-server.get('/', async (req, res) => {
+server.get('/api/users', async (req, res) => {
     try {
         console.log("message of the day posting")
         res.status(200).json({messageOfTheDay: process.env.MOTD})
@@ -15,5 +15,32 @@ server.get('/', async (req, res) => {
         res.status(500).json({error: 'Cannot retrieve the Message'})
     }
 })
+
+server.post('/api/register', async (req, res) => {
+    try {
+        const newUser = req.body
+        console.log("registering users")
+        res.status(201).json({users: newUser})
+    } catch {
+        console.log('ERROR', error);
+        res.status(500).json({error: 'Cannot retrieve the Message'})
+    }
+})
+
+server.post('/api/register', async (req, res) => {
+    const newUser = req.body
+    try {
+        console.log("logging in user")
+        if(!newUser){
+            res.status(422).json({ message:"needs to be a user"});
+        } else {
+            res.status(201).json({users: newUser, message: "Welcome!"})
+        }
+    } catch {
+        console.log('ERROR', error);
+        res.status(500).json({error: 'Cannot retrieve the Message'})
+    }
+})
+
 
 module.exports = server;
